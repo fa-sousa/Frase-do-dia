5d94130f0ae21dde1343884e14e317a6bf3bef63
Fatima Ferreira de Sousa <fatimaferreiradesousa@8849-NB.local>
2021-01-19 00:25:56 -0300

    configura botão e frases

--- app/src/main/java/com/fatimasousa/frasesdodia/MainActivity.kt
+++ app/src/main/java/com/fatimasousa/frasesdodia/MainActivity.kt
@@ -2,12 +2,43 @@ package com.fatimasousa.frasesdodia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var texto: TextView
+    val frases = arrayOf(
+        "O ignorante afirma, o sábio duvida, o sensato reflete.",
+        "Se a tranquilidade da água permite refletir as coisas, o que não poderá a tranquilidade do espírito?",
+        "Tente mover o mundo – o primeiro passo será mover a si mesmo.",
+        "Não corrigir nossas falhas é o mesmo que cometer novos erros."
+    )
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.textoFrase) as TextView
+        button = findViewById(R.id.button) as Button
+
+
+        button.setOnClickListener {
+            gerarFrase()
+        }
     }
+
+
+    private fun gerarFrase() {
+
+        val totalIntensArray = frases.size
+        val numeroAleatorio = Random().nextInt(totalIntensArray)
+
+        texto.setText( frases[ numeroAleatorio ])
+
+    }
+
 
 }
